4af01074dad011d440649139c32f258865ea246d
Dave Raring <d@7pi.co>
2023-02-15 12:46:46 -0600

    updating context

--- _context.tf
+++ _context.tf
@@ -1,3 +1,25 @@
+## ----------------------------------------------------------------------------
+##  Copyright 2023 SevenPico, Inc.
+##  Copyright 2020 Cloud Posse, LLC
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##     http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+## ----------------------------------------------------------------------------
+
+## ----------------------------------------------------------------------------
+##  _context.tf
+##  Copied and adapted from terraform-null-label (https://github.com/cloudposse/terraform-null-label/blob/master/exports/context.tf)
+## ----------------------------------------------------------------------------
+
 #
 # ONLY EDIT THIS FILE IN github.com/cloudposse/terraform-null-label
 # All other instances of this file should be a copy of that one
@@ -21,8 +43,8 @@
 #
 
 module "context" {
-  source  = "app.terraform.io/SevenPico/context/null"
-  version = "1.0.2" # requires Terraform >= 0.13.0
+  source  = "SevenPico/context/null"
+  version = "2.0.0" # requires Terraform >= 0.13.0
 
   enabled             = var.enabled
   namespace           = var.namespace
